3751eb0ac4071651305ea46250ccc35e5f52d126
Thea Paulsen <thea-remman.paulsen@geomatikk.no>
2024-06-21 10:44:23 +0200

    Tableview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+  {
+    path: '',
+    redirectTo: 'home-page',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'showCable',
+    loadComponent: () =>
+      import('./show-cable/show-cable.component').then((m) => m.ShowCableComponent),
+  },
+  {
+    path: 'project-list',
+    loadComponent: () =>
+      import('./project-list/project-list.component').then((m) => m.ProjectListComponent),
+  },
+
+  {
+    path: 'home-page',
+    loadComponent: () =>
+      import('./home-page/home-page.component').then((m) => m.HomePageComponent),
+  },
+  
+];
---
+++ src/app/project-list/list.interface.ts
@@ -0,0 +1,6 @@
+export interface ListInfo {
+    id: number
+    name: string
+    email: string
+  }
+  
