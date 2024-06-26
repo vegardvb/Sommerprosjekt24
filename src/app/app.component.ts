e2da79810fcc8cc8ec6fa6ca562e83191333c81d
Thea Paulsen <thea-remman.paulsen@geomatikk.no>
2024-06-26 13:23:01 +0200

    Project table with data from backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
 import { CesiumDirective } from './cesium.directive';
-
 import { ProjectListComponent } from './project-list/project-list.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -20,5 +18,6 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent,
   ],
   templateUrl: './app.component.html',
+  providers: [],
 })
 export class AppComponent {}
---
+++ src/app/data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Inquiry } from '../models/inquiry-interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private apiUrl = 'http://127.0.0.1:8000/inquiery';
+
+  constructor(private http: HttpClient) {}
+
+  getData(): Observable<Inquiry[]> {
+    return this.http
+      .get<Inquiry[]>(this.apiUrl)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: Error): Observable<never> {
+    console.error('An error occurred:', error.message);
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
+  }
+}
