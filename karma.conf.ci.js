5cac3484f3b4cca8256dfafb5538bf46606418c8
Vegard Vaeng Bernhardsen <vegard-vaeng.bernhardsen@geomatikk.no>
2024-06-28 15:12:25 +0200

    chore: Update `.gitlab/frontend-test.gitlab-ci.yml` to use node:18-alpine image instead of trion/ng-cli-karma

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -1,6 +1,4 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
+// karma.conf.ci.js
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -8,7 +6,6 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
@@ -21,23 +18,16 @@ module.exports = function (config) {
       },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
-    },
     coverageReporter: {
       dir: require('path').join(
         __dirname,
         './coverage/cable_network_visualization'
       ),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'lcovonly' },
-        { type: 'text-summary' },
-      ],
+      reporters: [{ type: 'lcovonly' }, { type: 'text-summary' }],
       fixWebpackSourcePaths: true,
       thresholds: {
-        emitWarning: true,
+        emitWarning: true, // set to `true` to not fail the test command when thresholds are not met
         global: {
           statements: 80,
           lines: 80,
@@ -46,7 +36,7 @@ module.exports = function (config) {
         },
       },
     },
-    reporters: ['progress', 'kjhtml', 'coverage'],
+    reporters: ['progress', 'coverage'],
     browsers: ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadlessCI: {
--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,4 @@
+// karma.conf.js
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -6,33 +7,32 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(
+        __dirname,
+        './coverage/cable_network_visualization'
+      ),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
       fixWebpackSourcePaths: true,
-      thresholds: {
-        emitWarning: true, // set to `true` to not fail the test command when thresholds are not met
-        global: {
-          statements: 80,
-          lines: 80,
-          branches: 80,
-          functions: 80,
-        },
-      },
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
     restartOnFileChange: true,
   });
 };
