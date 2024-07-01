455f42331dd863c18d0130e6ca12fc5fdbf44ad9
Petter Edvardsen <petter.edvardsen@geomatikk.no>
2024-07-01 15:09:17 +0200

    Update of test module on redundant test for testing pipeline.

--- src/app/services/cable-measurement.service.spec.ts
+++ src/app/services/cable-measurement.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
-import { CableMeasurementService } from '../cable-measurement.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CableMeasurementService } from './cable-measurement.service';
 
 describe('CableMeasurementService', () => {
   let service: CableMeasurementService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(CableMeasurementService);
   });
 
