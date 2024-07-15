c906e1b86cff9ca8e09c6efba80c5b3d755b405a
Petter Edvardsen <petter.edvardsen@geomatikk.no>
2024-07-15 12:35:00 +0200

    Functional boundary, working_area and 3D measurements as GeoJSON endpoints.

--- backend/sql/views/inquiry/view_points_of_cables_as_geojson_by_inquiry.sql
+++ backend/sql/views/inquiry/view_points_of_cables_as_geojson_by_inquiry.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE VIEW "Points_of_Cables_as_GeoJSON_by_Inquiry" AS
 SELECT inquiry_id,
        json_agg(point_cable_geojson.cable_points_geojson) AS geojson
-FROM "Points_of_Cables_as_GeoJSON"            point_cable_geojson
+FROM "Points_of_Cables_as_GeoJSON_3D"         point_cable_geojson
          RIGHT JOIN "Measurements_by_Inquiry" measurement_inquiry
                     ON measurement_inquiry.measurement_id = point_cable_geojson.cable_measurement_id
          RIGHT JOIN "Cables_by_Measurement"   cable_measurements
