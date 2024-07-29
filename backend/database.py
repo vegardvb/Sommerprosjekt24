31866b4bfb7f4d23595ee0013692bf080f089ac4
Vegard Vaeng Bernhardsen <vegard-vaeng.bernhardsen@geomatikk.no>
2024-07-29 14:57:43 +0200

    chore: Refactor file paths to use single quotes consistently, with organize of imports

--- backend/database.py
+++ backend/database.py
@@ -3,36 +3,37 @@ This module provides functionality for connecting to the database.
 """
 
 import os
+
+from dotenv import load_dotenv
 from sqlalchemy import (
+    Column,
+    DateTime,
+    Float,
+    Integer,
+    MetaData,
     ResultProxy,
+    String,
+    Table,
     create_engine,
     text,
-    MetaData,
-    Table,
-    Column,
-    Integer,
-    String,
-    Float,
-    DateTime,
 )
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Database credentials
-db_name = os.getenv("DB_NAME")
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-db_host = os.getenv("DB_HOST")
-db_port = os.getenv("DB_PORT")
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
 
 # Create connection to database
-DATABASE_URL = f"postgresql://{db_user}:{
-    db_password}@{db_host}:{db_port}/{db_name}"
-SCHEMA = "analytics_cable_measurement_inquiries"
-SCHEMA_PUBLIC = "public"
+DATABASE_URL = f'postgresql://{db_user}:{
+    db_password}@{db_host}:{db_port}/{db_name}'
+SCHEMA = 'analytics_cable_measurement_inquiries'
+SCHEMA_PUBLIC = 'public'
 
 # Engine for executing queries
 engine = create_engine(DATABASE_URL, echo=True, future=True)
@@ -46,7 +47,7 @@ def get_db():
     """
     with engine.connect() as connection:
         connection = connection.execution_options(mapper=ResultProxy.mappings)
-        connection.execute(text(f"SET search_path TO {SCHEMA}"))
+        connection.execute(text(f'SET search_path TO {SCHEMA}'))
         yield connection
 
 
@@ -58,7 +59,7 @@ def get_db_public():
     """
     with engine.connect() as connection:
         connection = connection.execution_options(mapper=ResultProxy.mappings)
-        connection.execute(text(f"SET search_path TO {SCHEMA_PUBLIC}"))
+        connection.execute(text(f'SET search_path TO {SCHEMA_PUBLIC}'))
         yield connection
 
 
@@ -67,16 +68,16 @@ metadata = MetaData()
 metadata = MetaData()
 
 ledningsmaaling_innmaaling_punkt = Table(
-    "ledningsmaaling_innmaaling_punkt",
+    'ledningsmaaling_innmaaling_punkt',
     metadata,
-    Column("id", Integer, primary_key=True, index=True),
-    Column("navn", String),
-    Column("bruker_id", Integer),
-    Column("geom", String),
-    Column("survey_geom", String),
-    Column("noyaktighet_z", Float),
-    Column("hoyde", Float),
-    Column("tidpunkt", DateTime),
-    Column("metadata", String),
+    Column('id', Integer, primary_key=True, index=True),
+    Column('navn', String),
+    Column('bruker_id', Integer),
+    Column('geom', String),
+    Column('survey_geom', String),
+    Column('noyaktighet_z', Float),
+    Column('hoyde', Float),
+    Column('tidpunkt', DateTime),
+    Column('metadata', String),
 )
 metadata.create_all(bind=engine)
