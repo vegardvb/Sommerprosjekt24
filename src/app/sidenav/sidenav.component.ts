149d0778b1409c5f82f58a4049d7498667ba3c9f
Jessica Husby <jessica.husby@geomatikk.no>
2024-08-01 14:14:10 +0200

    dynamic cables

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -124,12 +124,17 @@ export class SidenavComponent implements OnInit {
    */
   updateSelectedEntity(entity: Entity) {
     this.selectedEntity = entity;
+
     const position = this.selectedEntity.position?.getValue(JulianDate.now());
     if (position) {
       const cartographic = Cartographic.fromCartesian(position);
       this.longitude = CesiumMath.toDegrees(cartographic.longitude);
       this.latitude = CesiumMath.toDegrees(cartographic.latitude);
       this.height = cartographic.height;
+    } else {
+      this.isEditing = false;
+      this.editingToggled.emit(this.isEditing);
+      console.log('other selected entity', this.selectedEntity);
     }
   }
 
